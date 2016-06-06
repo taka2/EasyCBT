ebe7545239ff841973490ab8335c0ec7da53e8c3
takahiro.shigemoto <takahiro.shigemoto>
2016-06-06 23:17:52 +0900

    Util#concatAnswersDescriptionは必要だった。

--- js/app/util/Util.js
+++ js/app/util/Util.js
@@ -4,6 +4,22 @@ function extractNumber(answersString) {
   return Number(result[1]);
 }
 
+// answers配列のdescriptionを改行でつないだ文字列を返す
+function concatAnswersDescription(answers) {
+  if(answers.length == 1) {
+    // 1件の場合は加工せず返す
+    return answers[0].description;
+  }
+
+  // 複数件の場合は加工して返す
+  var resultStringArray = [];
+  for(var i=0; i<answers.length; i++) {
+    resultStringArray.push("(" + (i+1) + ") " + answers[i].description);
+  }
+
+  return resultStringArray.join(' ');
+}
+
 // 正答率を計算する
 function calcPercentageOfCorrectAnswers(questionCount, correctAnswersCount) {
   var percentage = (correctAnswersCount / questionCount * 100);
